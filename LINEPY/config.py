6aa1fc93942baac86881bf33160b962d64920bbe
chemot <40890269+chemot@users.noreply.github.com>
2018-07-20 13:50:13 +0700

    Update config.py

--- LINEPY/config.py
+++ LINEPY/config.py
@@ -28,10 +28,10 @@ class Config(object):
     }
 
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
-    APP_VER     = '8.0.3'
+    APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'CHROMEOS'
-    SYSTEM_VER  = '10.12.0'
+    SYSTEM_VER  = '2.1.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
